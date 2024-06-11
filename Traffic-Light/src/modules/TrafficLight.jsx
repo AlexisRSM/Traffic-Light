71d2521a333760ec311ab7f6d868869e6f92625d
AlexisRSM <alexismendes98@hotmail.com>
2024-06-12 00:14:46 +0100

    In Work

--- Traffic-Light/src/modules/TrafficLight.jsx
+++ Traffic-Light/src/modules/TrafficLight.jsx
@@ -1,12 +1,32 @@
 
 function TrafficLight() {
+    const [color, setColor] = useState();
+
+function setColor(color) {
+    switch (color) {
+        case "red":
+            let red_glow = document.querySelector(".red");
+            break;
+        case "yellow":
+            setColor("yellow");
+            break;
+        case "green":
+            setColor("green");
+            break;
+        default:
+            setColor("red");
+            break;
+    }
+}
+
+
     return (
         <>
             <div className="traffic-light">
                 <div className="container">
-                    <div className="red light"></div>
-                    <div className="yellow light"></div>
-                    <div className="green light"></div>
+                    <div onClick={setColor("red")} className="red light"></div>
+                    <div onClick={setColor("yellow")} className="yellow light"></div>
+                    <div onClick={setColor("green")} className="green light"></div>
                 </div>
                 <div className="pole"></div>
             </div>
